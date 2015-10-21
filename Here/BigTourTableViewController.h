fe65711a5ae1f519985d223d99ab1b3a51d64b2f
Steve Spencer <sspencer@playnetwork.com>
2015-10-21 08:16:02 -0700

    Initial checkin

--- Here/BigTourTableViewController.h
+++ Here/BigTourTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BigTourTableViewController.h
 //  Here
 //
 //  Created by Steve Spencer on 10/21/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BigTourTableViewController : UITableViewController
 
 @end
-
---
+++ Here/BigTourTableViewController.m
@@ -0,0 +1,64 @@
+//
+//  BigTourTableViewController.m
+//  Here
+//
+//  Created by Steve Spencer on 10/21/15.
+//  Copyright © 2015 Steve Spencer. All rights reserved.
+//
+
+#import "BigTourTableViewController.h"
+
+@interface BigTourTableViewController ()
+
+@end
+
+@implementation BigTourTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.title = @"Tour 66";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"loading" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
